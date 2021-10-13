bb5ef4c138fd94a43f2f935c0b5652c1cb4521ed
danielnathan <danielnatham@gmail.com>
2021-10-12 21:02:58 -0300

    feat: Adicionando Rotas de Create e Read, Form e Errors no Form

--- src/main/java/com/nathan/daniel/Code/Blog/controller/CodeblogController.java
+++ src/main/java/com/nathan/daniel/Code/Blog/controller/CodeblogController.java
@@ -2,14 +2,22 @@ package com.nathan.daniel.Code.Blog.controller;
 
 import com.nathan.daniel.Code.Blog.model.Post;
 import com.nathan.daniel.Code.Blog.service.CodeblogService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
+@Slf4j
 @Controller
 public class CodeblogController {
 
@@ -17,14 +25,53 @@ public class CodeblogController {
     CodeblogService codeblogService;
 
     @GetMapping("/posts")
-    public ModelAndView getPosts(){
+    public ModelAndView getPosts() {
 
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.findAll();
 
-        mv.addObject("posts",posts);
+        mv.addObject("posts", posts);
 
         return mv;
+    }
+
+    @GetMapping("/posts/{id}")
+    public ModelAndView getPostDetails(@PathVariable("id") Long id) {
+
+        ModelAndView mv = new ModelAndView("postDetails");
+
+        Post post = codeblogService.findById(id);
+
+        mv.addObject("post", post);
+
+        return mv;
+    }
+
+    @GetMapping("/newpost")
+    public ModelAndView getPostForm() {
+
+        ModelAndView mv = new ModelAndView("postForm");
+        mv.addObject("post", new Post());
+
+        return mv;
+    }
+
+    @PostMapping("/newpost")
+    public ModelAndView savePost(@Valid Post post, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if(bindingResult.hasErrors()){
+
+            log.error("Errors {}", bindingResult.getErrorCount());
+            log.error("Post {}", post.toString());
+
+            return new ModelAndView("postForm");
+
+        }
+
+        post.setCreation(LocalDate.now());
+        codeblogService.save(post);
+
+        return new ModelAndView("redirect:/posts");
     }
 
 }
--- src/main/java/com/nathan/daniel/Code/Blog/model/Post.java
+++ src/main/java/com/nathan/daniel/Code/Blog/model/Post.java
@@ -1,15 +1,13 @@
 package com.nathan.daniel.Code.Blog.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -22,16 +20,16 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotBlank
+    @NotBlank(message = "Não pode estar vazio!")
     private String title;
 
-    @NotBlank
+    @NotBlank(message = "Não pode estar vazio!")
     private String author;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate creation;
 
-    @NotBlank
+    @NotBlank(message = "Não pode estar vazio!")
     @Lob
     private  String text;
 
